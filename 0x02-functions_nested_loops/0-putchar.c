fcafe132d93c480a71091c6da3bf5f24530041f8
hiamenoch <sciencechi@gmail.com>
2022-09-15 15:09:26 -0700

    Put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*main - prints the word Holberton
+*main - prints putchar
 *
 *Return: 0 on success execution.
 */
@@ -11,12 +11,12 @@ int main(void)
 	char s[] = "_putchar";
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (i<8)
 	{
 		_putchar(s[i]);
 		i++;
 	}
-	_putchar(10);
+	_putchar(s[8]);
 
 	return (0);
 }
